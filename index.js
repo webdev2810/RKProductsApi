045af39321400127aadbfa707bf57dc7621bdbd5
webdev2810 <138306196+webdev2810@users.noreply.github.com>
2023-11-24 01:33:23 +0530

    rk jewells api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 
 const express = require("express");
 const cors = require('cors')
@@ -5,9 +6,12 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use(cors());
 
-const apiData = require("./productData.json");
+// const apiData = require("./productData.json");
+
+const products = require("./productData.json")
 const singleApiData = require("./singleProductData.json");
 
 
@@ -16,16 +20,77 @@ app.get("/", (req, res) => {
   res.send("Now, Productss are live")
 });
 
-app.get("/products", (req, res) => {
-  res.send(apiData)
+app.get("/api/products", (req, res) => {
+  res.send(products)
 })
-app.get("/products/singleproduct/:id",  (req, res) => {
 
-
-  // let sProduct = singleApiData.findById(`${id}`);
+app.get('/api/products/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const product = products.find(p => p.id === productId);
   
-  res.send({singleApiData});
-})
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  });
+// app.get("/products/singleproduct/:id",  (req, res) => {
+
+
+//   // let sProduct = singleApiData.findById(`${id}`);
+  
+//   res.send({singleApiData});
+// })
 app.listen(port, () => {
   console.log(`RKJwells API Keys listening on port`);
 });
+
+
+// // index.js
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+// const app = express();
+// const PORT = 3001;
+
+// app.use(cors());
+
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Diamond Necklace',
+//     price: 499.99,
+//     imageUrl: 'diamond_necklace.jpg',
+//     description: 'Beautiful diamond necklace for any occasion.',
+//   },
+//   {
+//     id: 2,
+//     name: 'Gold Earrings',
+//     price: 199.99,
+//     imageUrl: 'gold_earrings.jpg',
+//     description: 'Elegant gold earrings to complement your style.',
+//   },
+//   // Add more products as needed
+// ];
+
+// // Get all products
+// app.get('/api/products', (req, res) => {
+//   res.json(products);
+// });
+
+// // Get details for a single product
+// app.get('/api/products/:id', (req, res) => {
+//   const productId = parseInt(req.params.id);
+//   const product = products.find(p => p.id === productId);
+
+//   if (product) {
+//     res.json(product);
+//   } else {
+//     res.status(404).json({ error: 'Product not found' });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
