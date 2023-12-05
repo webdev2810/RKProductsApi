e6d2a53a059387475a663edb343edd9f65c4d12c
webdev2810 <138306196+webdev2810@users.noreply.github.com>
2023-12-06 02:48:00 +0530

    rk jewells api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
+
 const express = require("express");
 const cors = require('cors')
 
@@ -13,28 +12,6 @@ app.use(cors());
 
 const products  = require("./productData.json")
 const singleApiData = require("./singleProductData.json");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // You may need to create the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, `${Date.now()}${ext}`);
-  },
-});
-
-const upload = multer({ storage });
-
-// Define a route for image upload
-app.post('/api/upload', upload.single('image'), (req, res) => {
-  // Handle the uploaded file
-  const uploadedFilePath = req.file.path;
-  console.log('Image uploaded to:', uploadedFilePath);
-
-  // Send a response to the client
-  res.status(200).json({ message: 'Image uploaded successfully' });
-});
 
 
 app.get("/", (req, res) => {
