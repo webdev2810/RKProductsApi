e982f4bc6eadd8f4c3ec6b0f923688ad389e803c
webdev2810 <138306196+webdev2810@users.noreply.github.com>
2023-12-20 00:43:47 +0530

    rk_jewells api

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:functions/index.js
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
 const express = require("express");
@@ -42,49 +41,4 @@ app.listen(port, () => {
 
 
 app.use('/.netlify/functions/api', app);
-=======
-const bodyParser = require('body-parser');
-const serverless = require('serverless-http');
-const express = require("express");
-const cors = require('cors')
-
-const app = express()
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(cors());
-
-
-const products  = require("../productData.json")
-const singleApiData = require("../singleProductData.json");
-
-
-app.get("/", (req, res) => {
-  res.send("Now, Productss are live")
-});
-
-app.get("/api/products", (req, res) => {
-  res.send(products)
-})
-
-
-app.get('/api/products/:id', (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = singleApiData.find(p => p.id === productId);
-  
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ error: 'Product not found' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`RKJwells API Keys listening on port`);
-});
-
-
-
-app.use('/.netlify/functions/api', app);
->>>>>>> 774dee2267bcf0e686f739d0eb31188217eb7108:index.js
 module.exports.handler = serverless(app);
